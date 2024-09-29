9630f959ebb05ecd861213d11145cf0814df3fd3
JasonH81 <122146172+JasonH81@users.noreply.github.com>
2024-09-29 01:04:23 -0400

    Added Cell class

---
+++ Match Three/src/jason/matchthree/Cell.java
@@ -0,0 +1,10 @@
+package jason.matchthree;
+
+import java.awt.Color;
+
+public class Cell {
+	
+	private static final Color[] COLORS = {Color.BLUE, Color.CYAN, Color.GREEN, Color.MAGENTA, Color.RED, Color.WHITE, Color.YELLOW};
+	public static final int SIZE = 50;
+
+}
